a21096c4ab6c26ce445c861bb9bb57a1625bdbdd
sarthax <sarthax@gmail.com>
2026-09-25 13:02:14 -0700

    Export DSP battlefield policy proposal backend

--- workbench/plugins/domain/__init__.py
+++ workbench/plugins/domain/__init__.py
@@ -2,7 +2,7 @@
 from .base import ContentArchetype, DomainPlugin, DomainPluginSpec, PluginContext, PluginFinding
 from .registry import DomainPluginRegistry
 from .planning import plugin_findings_to_actions
-from .battlefield_dsp import DspBattlefieldMember, DspBattlefieldMembershipProposal, propose_dsp_battlefield_membership
+from .battlefield_dsp import DspBattlefieldMember, DspBattlefieldMembershipProposal, DspBattlefieldPolicyProposal, propose_dsp_battlefield_membership, propose_dsp_battlefield_policy
 from .builtin import (
     ARCHETYPES,
     AssaultPlugin,
@@ -15,6 +15,6 @@ from .builtin import (
 
 __all__=[
     "ContentArchetype","DomainPlugin","DomainPluginSpec","PluginContext","PluginFinding",
-    "DomainPluginRegistry","plugin_findings_to_actions","DspBattlefieldMember","DspBattlefieldMembershipProposal","propose_dsp_battlefield_membership","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
+    "DomainPluginRegistry","plugin_findings_to_actions","DspBattlefieldMember","DspBattlefieldMembershipProposal","DspBattlefieldPolicyProposal","propose_dsp_battlefield_membership","propose_dsp_battlefield_policy","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
     "MultiZoneProgressionPlugin","MinigamePlugin","AssaultPlugin","default_registry",
 ]
