7b635109d1700eb71e1fa6ce96828823fcd9897c
sarthax <sarthax@gmail.com>
2026-09-25 15:44:04 -0700

    Expose client binary diff service

--- workbench/client/__init__.py
+++ workbench/client/__init__.py
@@ -2,4 +2,6 @@
 
 from .binary_index import BinaryFormatError, PEImage, index_binary, write_index
 
-__all__=["BinaryFormatError","PEImage","index_binary","write_index"]
+__all__=["BinaryFormatError","PEImage","index_binary","write_index" ,"diff_binary_indexes"]
+
+from .binary_diff import diff_binary_indexes
