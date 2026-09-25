fdb7d08ecaf4f338fea2263cbae692ddd83c54e5
sarthax <sarthax@gmail.com>
2026-09-25 16:04:40 -0700

    Export deep client binary helpers

--- workbench/client/__init__.py
+++ workbench/client/__init__.py
@@ -2,6 +2,6 @@
 
 from .binary_index import BinaryFormatError, PEImage, index_binary, write_index
 
-__all__=["BinaryFormatError","PEImage","index_binary","write_index" ,"diff_binary_indexes"]
+__all__=["BinaryFormatError","BinaryAnalysisError","PEImage","index_binary","write_index","diff_binary_indexes","parse_hex_pattern","byte_search","xrefs","function_candidates"]
 
 from .binary_diff import diff_binary_indexes
