10114d9bd0ffaba42f9c6ecc2c7949965f291cb2
sarthax <sarthax@gmail.com>
2026-09-25 14:30:43 -0700

    Add research service package

---
+++ workbench/research/__init__.py
@@ -0,0 +1,4 @@
+"""Evidence-aware research services."""
+from .session import PERMISSION_PROFILES, ResearchSession, ResearchSessionStore
+
+__all__=["PERMISSION_PROFILES","ResearchSession","ResearchSessionStore"]
