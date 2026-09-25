c85bbeb24d80269502a22642fdd916f348d021ba
sarthax <sarthax@gmail.com>
2026-09-25 13:39:28 -0700

    Export DSP battlefield representation planner

--- workbench/plugins/domain/__init__.py
+++ workbench/plugins/domain/__init__.py
@@ -2,7 +2,7 @@
 from .base import ContentArchetype, DomainPlugin, DomainPluginSpec, PluginContext, PluginFinding
 from .registry import DomainPluginRegistry
 from .planning import plugin_findings_to_actions
-from .battlefield_dsp import DspBattlefieldMember, DspBattlefieldMembershipProposal, DspBattlefieldPolicyProposal, DspBattlefieldCallbackSurface, DspBattlefieldCallbackAdaptationPlan, propose_dsp_battlefield_membership, propose_dsp_battlefield_policy, analyze_dsp_battlefield_callbacks, plan_dsp_battlefield_callback_adaptation, generated_outputs_for_dsp_battlefield
+from .battlefield_dsp import DspBattlefieldMember, DspBattlefieldMembershipProposal, DspBattlefieldPolicyProposal, DspBattlefieldCallbackSurface, DspBattlefieldCallbackAdaptationPlan, DspBattlefieldRepresentationPlan, propose_dsp_battlefield_membership, propose_dsp_battlefield_policy, analyze_dsp_battlefield_callbacks, plan_dsp_battlefield_callback_adaptation, plan_dsp_battlefield_representation, generated_outputs_for_dsp_battlefield
 from .battlefield_lsb import LsbBattlefieldPolicySurface, LsbBattlefieldMobGroups, extract_lsb_battlefield_policy, extract_lsb_mission_level_cap, extract_lsb_battlefield_mob_groups
 from .battlefield_validation import GeneratedSqlValidation, validate_dsp_battlefield_proposals
 from .builtin import (
@@ -17,6 +17,6 @@ from .builtin import (
 
 __all__=[
     "ContentArchetype","DomainPlugin","DomainPluginSpec","PluginContext","PluginFinding",
-    "DomainPluginRegistry","plugin_findings_to_actions","DspBattlefieldMember","DspBattlefieldMembershipProposal","DspBattlefieldPolicyProposal","DspBattlefieldCallbackSurface","DspBattlefieldCallbackAdaptationPlan","propose_dsp_battlefield_membership","propose_dsp_battlefield_policy","analyze_dsp_battlefield_callbacks","plan_dsp_battlefield_callback_adaptation","generated_outputs_for_dsp_battlefield","LsbBattlefieldPolicySurface","LsbBattlefieldMobGroups","extract_lsb_battlefield_policy","extract_lsb_mission_level_cap","extract_lsb_battlefield_mob_groups","GeneratedSqlValidation","validate_dsp_battlefield_proposals","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
+    "DomainPluginRegistry","plugin_findings_to_actions","DspBattlefieldMember","DspBattlefieldMembershipProposal","DspBattlefieldPolicyProposal","DspBattlefieldCallbackSurface","DspBattlefieldCallbackAdaptationPlan","DspBattlefieldRepresentationPlan","propose_dsp_battlefield_membership","propose_dsp_battlefield_policy","analyze_dsp_battlefield_callbacks","plan_dsp_battlefield_callback_adaptation","plan_dsp_battlefield_representation","generated_outputs_for_dsp_battlefield","LsbBattlefieldPolicySurface","LsbBattlefieldMobGroups","extract_lsb_battlefield_policy","extract_lsb_mission_level_cap","extract_lsb_battlefield_mob_groups","GeneratedSqlValidation","validate_dsp_battlefield_proposals","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
     "MultiZoneProgressionPlugin","MinigamePlugin","AssaultPlugin","default_registry",
 ]
