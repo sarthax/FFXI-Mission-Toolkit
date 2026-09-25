29bcc9f05001b2dad513837437faadec521cc574
sarthax <sarthax@gmail.com>
2026-09-25 13:04:58 -0700

    Export LSB battlefield policy extraction

--- workbench/plugins/domain/__init__.py
+++ workbench/plugins/domain/__init__.py
@@ -3,6 +3,7 @@ from .base import ContentArchetype, DomainPlugin, DomainPluginSpec, PluginContex
 from .registry import DomainPluginRegistry
 from .planning import plugin_findings_to_actions
 from .battlefield_dsp import DspBattlefieldMember, DspBattlefieldMembershipProposal, DspBattlefieldPolicyProposal, DspBattlefieldCallbackSurface, propose_dsp_battlefield_membership, propose_dsp_battlefield_policy, analyze_dsp_battlefield_callbacks
+from .battlefield_lsb import LsbBattlefieldPolicySurface, extract_lsb_battlefield_policy, extract_lsb_mission_level_cap
 from .builtin import (
     ARCHETYPES,
     AssaultPlugin,
@@ -15,6 +16,6 @@ from .builtin import (
 
 __all__=[
     "ContentArchetype","DomainPlugin","DomainPluginSpec","PluginContext","PluginFinding",
-    "DomainPluginRegistry","plugin_findings_to_actions","DspBattlefieldMember","DspBattlefieldMembershipProposal","DspBattlefieldPolicyProposal","DspBattlefieldCallbackSurface","propose_dsp_battlefield_membership","propose_dsp_battlefield_policy","analyze_dsp_battlefield_callbacks","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
+    "DomainPluginRegistry","plugin_findings_to_actions","DspBattlefieldMember","DspBattlefieldMembershipProposal","DspBattlefieldPolicyProposal","DspBattlefieldCallbackSurface","propose_dsp_battlefield_membership","propose_dsp_battlefield_policy","analyze_dsp_battlefield_callbacks","LsbBattlefieldPolicySurface","extract_lsb_battlefield_policy","extract_lsb_mission_level_cap","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
     "MultiZoneProgressionPlugin","MinigamePlugin","AssaultPlugin","default_registry",
 ]
