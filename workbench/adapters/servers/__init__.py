00eba2b61bbca79b3645ec27dc2a79e8cb01c6fa
sarthax <sarthax@gmail.com>
2026-09-25 10:51:54 -0700

    adapters: export server adapter API

--- workbench/adapters/servers/__init__.py
+++ workbench/adapters/servers/__init__.py
@@ -1,1 +1,10 @@
-"""Workbench subsystem package."""
+"""FFXI server-source adapter contracts and built-in profiles."""
+from .base import AdapterProbe, SchemaProfile, ServerAdapter, TableShape
+from .profile_adapters import DSPAdapter, LSBAdapter, TopazAdapter, adapter_for
+from .profiles import DSP, LSB, TOPAZ
+
+__all__ = [
+    "AdapterProbe", "SchemaProfile", "ServerAdapter", "TableShape",
+    "DSPAdapter", "LSBAdapter", "TopazAdapter", "adapter_for",
+    "DSP", "LSB", "TOPAZ",
+]
