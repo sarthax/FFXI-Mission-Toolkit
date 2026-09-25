1b690d736f794d394c92178cb101d52722ecce32
sarthax <sarthax@gmail.com>
2026-09-25 15:40:28 -0700

    Expose client binary indexing package

--- workbench/client/__init__.py
+++ workbench/client/__init__.py
@@ -1,1 +1,5 @@
 """Client DAT/EXE/DLL capability and synchronization tooling."""
+
+from .binary_index import BinaryFormatError, PEImage, index_binary, write_index
+
+__all__=["BinaryFormatError","PEImage","index_binary","write_index"]
