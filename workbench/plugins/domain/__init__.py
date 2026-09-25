acd01cef24742e651b6bb1a4af18a7f6eb8667f0
sarthax <sarthax@gmail.com>
2026-09-25 13:00:42 -0700

    Export DSP battlefield membership proposal backend

--- workbench/plugins/domain/__init__.py
+++ workbench/plugins/domain/__init__.py
@@ -2,6 +2,7 @@
 from .base import ContentArchetype, DomainPlugin, DomainPluginSpec, PluginContext, PluginFinding
 from .registry import DomainPluginRegistry
 from .planning import plugin_findings_to_actions
+from .battlefield_dsp import DspBattlefieldMember, DspBattlefieldMembershipProposal, propose_dsp_battlefield_membership
 from .builtin import (
     ARCHETYPES,
     AssaultPlugin,
@@ -14,6 +15,6 @@ from .builtin import (
 
 __all__=[
     "ContentArchetype","DomainPlugin","DomainPluginSpec","PluginContext","PluginFinding",
-    "DomainPluginRegistry","plugin_findings_to_actions","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
+    "DomainPluginRegistry","plugin_findings_to_actions","DspBattlefieldMember","DspBattlefieldMembershipProposal","propose_dsp_battlefield_membership","ARCHETYPES","BattlefieldFamilyPlugin","QuestMissionPlugin",
     "MultiZoneProgressionPlugin","MinigamePlugin","AssaultPlugin","default_registry",
 ]
