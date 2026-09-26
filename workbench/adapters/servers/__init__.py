59a431ca0f5d36df7ddf8c9825ff61a2a517d991
sarthax <sarthax@gmail.com>
2026-09-25 17:26:25 -0700

    Export logical schema coverage helpers

--- workbench/adapters/servers/__init__.py
+++ workbench/adapters/servers/__init__.py
@@ -2,9 +2,11 @@
 from .base import AdapterProbe, FieldMapping, LogicalRecord, SchemaProfile, ServerAdapter, TableShape
 from .profile_adapters import CustomForkAdapter, DSPAdapter, LSBAdapter, TopazAdapter, TopazNextAdapter, adapter_for
 from .profiles import DSP, LSB, TOPAZ, TOPAZ_NEXT
+from .schema_coverage import TableMappingCoverage, compare_profile_coverage, profile_mapping_coverage, table_mapping_coverage
 
 __all__ = [
     "AdapterProbe", "FieldMapping", "LogicalRecord", "SchemaProfile", "ServerAdapter", "TableShape",
     "CustomForkAdapter", "DSPAdapter", "LSBAdapter", "TopazAdapter", "TopazNextAdapter", "adapter_for",
     "DSP", "LSB", "TOPAZ", "TOPAZ_NEXT",
+    "TableMappingCoverage", "compare_profile_coverage", "profile_mapping_coverage", "table_mapping_coverage",
 ]
